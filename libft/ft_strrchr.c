076a94320a010dabc593f0a9e680f6c3883a8ddd
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-07-26 12:26:54 +0200

    finish ft_strrchr.c

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/26 11:58:54 by pspuhler          #+#    #+#             */
+/*   Updated: 2026/07/26 12:26:35 by pspuhler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char			*p;
+	unsigned char	ch;
+	size_t			len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	ch = c;
+	p = (char *)s + len;
+	if (ch == '\0')
+	{
+		return (p++);
+	}
+	while (p >= s)
+	{
+		if (*p == ch)
+		{
+			return (p);
+		}
+		p--;
+	}
+	return (0);
+}
+
+/*
+int	main(void)
+{
+	const char	text[50] = "Hola Malaga";
+	char	*result;
+
+	result = ft_strrchr(text, 'l');
+	printf("%s", result);
+	return (0);
+}
+*/
