c14ebb2109ce54b2976f69476ff91575a11a6a79
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-07-27 19:37:02 +0200

    finish ft_atoi.c

---
+++ libft/ft_atoi.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/27 19:16:15 by pspuhler          #+#    #+#             */
+/*   Updated: 2026/07/27 19:36:48 by pspuhler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	result;
+	int	sign;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+	{
+		i++;
+	}
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while ((nptr[i] >= '0' && nptr[i] <= '9') && nptr[i] != '\0')
+	{
+		result = (result * 10) + (nptr[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
+
+/*
+int	main(void)
+{
+	const char	text[] = "+42";
+	int	result;
+
+	result = ft_atoi(text);
+	printf("%i", result);
+	return (0);
+}
+*/
