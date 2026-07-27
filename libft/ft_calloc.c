9b9b2aad4ac0cba12b2d78bb6804adea0c3f0d44
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-07-27 22:56:10 +0200

    finish ft_calloc.c

---
+++ libft/ft_calloc.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/27 19:37:36 by pspuhler          #+#    #+#             */
+/*   Updated: 2026/07/27 22:55:52 by pspuhler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Without ft_bzero()
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void			*r;
+	unsigned char	*rr;
+	size_t			i;
+
+	r = malloc(count * size);
+	if (!r)
+		return (NULL);
+	i = 0;
+	rr = (unsigned char *)r;
+	while (i < (count * size))
+	{
+		rr[i] = '\0';
+		i++;
+	}
+	return (r);
+}
+
+/*
+int	main(void)
+{
+	char	*result;
+
+	result = ft_calloc(5, 1);
+	result[0] = 'A';
+	result[1] = 'B';
+	printf("%s", result);
+	free(result);
+	return (0);
+}
+*/
