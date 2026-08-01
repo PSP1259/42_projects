2c9ab6eccdc60e7588699000bb1ddd2aae9f322d
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-01 13:31:17 +0200

    finish ft_strdup.c

---
+++ libft/ft_strdup.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/01 13:06:13 by pspuhler          #+#    #+#             */
+/*   Updated: 2026/08/01 13:30:53 by pspuhler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Without strlcpy (line 32)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s)
+{
+	char	*d;
+	size_t	len;
+	size_t	i;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	len++;
+	d = malloc(len * sizeof(char));
+	if (!d)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		d[i] = s[i];
+		i++;
+	}
+	d[i] = '\0';
+	return (d);
+}
+
+/*
+int	main(void)
+{
+	const char	text[] = "Hola Malaga";
+	const char	*result;
+
+	result = ft_strdup(text);
+	printf("%s", result);
+	return (0);
+}
+*/
