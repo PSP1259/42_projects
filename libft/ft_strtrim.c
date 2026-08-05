41f819731fef9fba0d7d6a1bb4667e4696ddf80a
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-05 18:25:29 +0200

    finish ft_strtrim.c

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,31 +6,75 @@
 /*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/01 23:01:15 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/08/01 23:12:38 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/08/05 18:25:15 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+- Failed by norminette: > 25 lines
+- line 41: ft_strdup
+- line 50: ft_strlen
+- line 58: ft_substr
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
+static int	is_set(char c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*s2;
-	int		len_1;
+	size_t	start;
+	size_t	end;
+	size_t	i;
+	char	*res;
 
 	if (!s1 || !set)
 		return (NULL);
-	len_1 = 0;
-	while (s1[len_1] = '\0')
-		len_1++;
-	if (len_1 == 0)
+	start = 0;
+	while (s1[start] && is_set(s1[start], set))
+		start++;
+	end = 0;
+	while (s1[end])
+		end++;
+	while (end > start && is_set(s1[end - 1], set))
+		end--;
+	res = malloc((end - start + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (start < end)
 	{
-		s2 = malloc(1 * sizeof(char));
-		if (!s2)
-			return (NULL);
-		s2[0] = '\0';
-		return (s3);
+		res[i] = s1[start];
+		i++;
+		start++;
 	}
-	if (*set == 0)
-		return (s1);
-	while 
+	res[i] = '\0';
+	return (res);
+}
+
+/*
+int	main(void)
+{
+	const char	s1[] = "    xxxHola Malaga   x  x";
+	const char	set[] = " x";
+	char *result;
+
+	result = ft_strtrim(s1, set);
+	printf("%s", result);
+	free(result);
+	return (0);
+}
+*/
