f68c9a4ac967c9d95eabebfc22de0ba8420fe884
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-15 17:43:50 +0200

    add main

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -6,17 +6,28 @@
 /*   By: pspuhler <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/19 16:57:48 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/07/19 17:06:26 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/08/15 17:43:12 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
-
 int	ft_isalnum(int c)
 {
-	if (ft_isalpha(c) || ft_isdigit(c))
-	{
+	if ((c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z')
+		|| (c >= '0' && c <= '9'))
 		return (c);
-	}
 	return (0);
 }
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	result; 
+
+	result = ft_isalnum(5);
+	printf("%i", result);
+	return (0);
+}
+*/
