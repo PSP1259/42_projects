fbcb8423baf4442e2b65c1165061cef31a70f0db
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-15 17:54:49 +0200

    add main

--- libft/ft_tolower.c
+++ libft/ft_tolower.c
@@ -6,7 +6,7 @@
 /*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 17:09:05 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/07/23 17:10:09 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/08/15 17:54:42 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,16 @@ int	ft_tolower(int c)
 		c += 32;
 	return (c);
 }
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	result;
+
+	result = ft_tolower(79);
+	printf("79 + 32 = %i", result);
+	return (0);
+}
+*/
