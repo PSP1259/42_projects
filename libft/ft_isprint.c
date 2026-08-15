787c3c4368a8b9b1775d8715fc98aceb207e3c2b
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-15 17:49:33 +0200

    add main

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -6,15 +6,26 @@
 /*   By: pspuhler <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 18:29:45 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/07/21 18:32:51 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/08/15 17:49:17 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isprint(int c)
 {
-	if (c >= 32 && <= 126)
-	{
+	if (c >= 32 && c <= 126)
 		return (c);
-	}
 	return (0);
 }
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	result;
+
+	result = ft_isprint(32);
+	printf("%i", result);
+	return (0);
+}
+*/
