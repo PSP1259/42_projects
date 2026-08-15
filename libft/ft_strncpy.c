467dfc5ff7b75a2f29b54980fa2bfb2889b8fd27
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-15 20:40:03 +0200

    finish ft_srncpy.c

---
+++ libft/ft_strncpy.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/15 20:25:20 by pspuhler          #+#    #+#             */
+/*   Updated: 2026/08/15 20:39:39 by pspuhler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char	*ft_strncpy(char *dst, char const *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	while (i < size)
+	{
+		dst[i] = '\0';
+		i++;
+	}
+	return (dst);
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char const	base[] = "Hola";
+	char		result[50] = {0};
+
+	ft_strncpy(result, base, 3);
+	printf("%s", result);
+	return (0);
+}
+*/
