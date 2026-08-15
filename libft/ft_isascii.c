039a52da4af4f2f83ac8098e2fb9520e95231956
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-15 17:46:41 +0200

    add main

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -6,19 +6,28 @@
 /*   By: pspuhler <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 18:21:19 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/07/21 18:26:37 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/08/15 17:46:23 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isascii(int c)
 {
 	if (c == 0)
-	{
 		return (1);
-	}
 	if (c >= 1 && c <= 127)
-	{
 		return (c);
-	}
 	return (0);
 }
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	result;
+
+	result = ft_isascii(111);
+	printf("%i", result);
+	return (0);
+}
+*/
