ca96aded37dcb23979b4f518c4e53e32812eeb1f
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-15 17:37:49 +0200

    add main

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,15 +6,28 @@
 /*   By: pspuhler <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/19 16:54:36 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/07/19 16:55:55 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/08/15 17:37:10 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isdigit(int c)
 {
-	if (c >= '0' && <= '9')
+	if (c >= '0' && c <= '9')
 	{
 		return (c);
 	}
 	return (0);
 }
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	result;
+
+	result = ft_isdigit(52);
+	printf("%i", result);
+	return (0);
+}
+*/
