e38ce5c1b02f1d5f577c99caee4ee0f984cf325e
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-16 22:42:16 +0200

    finish ft_strncat.c

---
+++ libft/ft_strncat.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/16 22:22:19 by pspuhler          #+#    #+#             */
+/*   Updated: 2026/08/16 22:42:03 by pspuhler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char	*strncat(char *dst, const char *src, size_t size)
+{
+	size_t	lend;
+	size_t	i;
+
+	lend = 0;
+	while (dst[lend] != '\0')
+		lend++;
+	i = 0;
+	while (i < size && src[i] != '\0')
+	{
+		dst[lend] = src[i];
+		lend++;
+		i++;
+	}
+	dst[lend] = '\0';
+	return (dst);
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	const char	base[] = "Malaga";
+	char		dst[50] = "Hola ";
+	char		*result;
+
+	result = strncat(dst, base, 3);
+	printf("%s", result);
+	return (0);
+}
+*/
