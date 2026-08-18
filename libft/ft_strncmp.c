57c58d6014fe6097a06370bfca661e0d88620413
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-18 12:03:17 +0200

    finish ft_strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,11 +6,9 @@
 /*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/26 12:27:31 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/07/26 12:52:23 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/08/18 12:03:00 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
@@ -19,15 +17,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] == s2[i] && i < n - 1
-		&& s1[i] != '\0')
-	{
+	while (s1[i] == s2[i] && i < n && s1[i] != '\0')
 		i++;
-	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 /*
+#include <stdio.h>
+
 int	main(void)
 {
 	const char s1[] = "Hola Malaga";
