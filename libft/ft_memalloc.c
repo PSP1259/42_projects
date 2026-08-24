cd18c3ec4fede22e5f120e1d4721c8d94f7d4aff
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-24 12:34:28 +0200

    finish ft_memalloc.c

---
+++ libft/ft_memalloc.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/24 12:13:01 by pspuhler          #+#    #+#             */
+/*   Updated: 2026/08/24 12:34:06 by pspuhler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	*ft_memalloc(size_t size)
+{
+	char	*new;
+	size_t	i;
+	
+	new = malloc(size);
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		new[i] = '\0';
+		i++;
+	}
+	return (new);
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*result;
+
+	result = ft_memalloc(8);
+	printf("%s", result);
+	free(new);
+	return (0);
+}
+*/
