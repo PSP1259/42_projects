6496157e1a89cfbe735fa03705af94d55d6299ed
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-28 16:32:42 +0200

    finish ft_strsub.c

---
+++ libft/ft_strsub.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/28 16:05:21 by pspuhler          #+#    #+#             */
+/*   Updated: 2026/08/28 16:32:20 by pspuhler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*new;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i] != '\0' && i < start)
+		i++;
+	if (i < start)
+		len = 0;
+	new = malloc(sizeof(char) * (len + 1));
+	if (!new)
+		return (0);
+	i = 0;
+	while (i < len && s[start + i] != '\0')
+	{
+		new[i] = s[start + i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char const	text[] = "Hola Malaga";
+	char		*result;
+
+	result = ft_strsub(text, 5, 3);
+	printf("%s", result);
+	free(result);
+	return (0);
+}
+*/
