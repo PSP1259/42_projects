083689999c1936088c1d9478ac470c3b10957e37
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-29 11:18:38 +0200

    finish ft_putnbr_fd.c

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,13 +6,9 @@
 /*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/14 18:36:47 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/08/14 18:49:56 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/08/29 11:18:28 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
-- Line 28, 35: ft_putchar_fd()
-*/
 
 #include <unistd.h>
 
@@ -36,9 +32,11 @@ void	ft_putnbr_fd(int n, int fd)
 	write(fd, &result, 1);
 }
 
+/*
 int	main(void)
 {
 	int	number = -123456;
 
 	ft_putnbr_fd(number, 1);
 }
+*/
