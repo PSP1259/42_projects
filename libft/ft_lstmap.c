8098ce2790891d8f6f625a8e58b552ff06a5c58c
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-08-31 16:43:17 +0200

    finish ft_lstmap.c

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,10 +6,42 @@
 /*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/30 17:33:56 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/08/30 17:34:36 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/08/31 16:42:51 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list	*head;
+	t_lsit	*tail;
+	t_list	*new_node;
+
+	if (!lst || !f)
+		return (0);
+	head = 0;
+	tail = 0;
+	while (lst)
+	{
+		new_node = f(lst);
+		if (!new_node)
+		{
+			while (head)
+			{
+				tail = head->next;
+				free(head->content;
+				free(head);
+				head = tail;
+			}
+			return (0);
+		}
+		if (!head)
+			head = new_node;
+		else
+			tail->next = new_node;
+		tail = new_node;
+		lst = lst->next;
+		return (head)
+	}
+}
