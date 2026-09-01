fcdd58c2fa07619a06f8c70bb0f4a72d04b23896
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-09-01 18:44:01 +0200

    add 2 functions

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,8 +6,19 @@
 /*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/01 15:42:37 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/09/01 15:42:39 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/01 18:19:06 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
 
+#include "../libft/libft.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int ft_print_char(va_list args);
+int	ft_print_str(va_list args);
+
+#endif
