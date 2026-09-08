a9de090727d7b4705b49ca7d1164f8a35f42b914
Philipp Spühler <214826864+PSP1259@users.noreply.github.com>
2026-09-08 16:04:09 +0200

    add ex19

--- piscine_reloaded/ex19/ft_sort_params.c
+++ piscine_reloaded/ex19/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: pspuhler <pspuhler@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 12:09:20 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/09/08 12:56:15 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/08 16:03:25 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,66 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int	main(int argc, char **argv)
+int	ft_strcmp(char *s1, char *s2)
 {
-	int		ic;
-	int		iv;
-	char	*tmp;
+	int	i;
 
-	if (argc < 1)
-		return (0);
-	ic = 0;
-	tmp = "Hello";
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_print_params(int argc, char **argv)
+{
+	int	ic;
+	int	iv;
+
+	ic = 1;
 	while (ic < argc)
 	{
-		if (argv[ic] > argv[ic + 1] && ic + 1 > argv)
-		{
-			tmp = argv[ic];
-			argv[ic] = argv[ic + 1];
-			argv[ic + 1] = tmp;
-		}
 		iv = 0;
 		while (argv[ic][iv] != '\0')
 		{
 			ft_putchar(argv[ic][iv]);
 			iv++;
 		}
-		ft_putchar('\n');
 		ic++;
+		ft_putchar('\n');
 	}
+}
+
+void	ft_sort_params(int argc, char **argv)
+{
+	int		ic;
+	int		swapped;
+	char	*tmp;
+
+	tmp = "Hello";
+	swapped = 1;
+	while (swapped)
+	{
+		swapped = 0;
+		ic = 0;
+		while (ic < argc - 1)
+		{
+			if (ft_strcmp(argv[ic], argv[ic + 1]) > 0)
+			{
+				tmp = argv[ic];
+				argv[ic] = argv[ic + 1];
+				argv[ic + 1] = tmp;
+				swapped = 1;
+			}
+			ic++;
+		}
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 1)
+		return (0);
+	ft_sort_params(argc, argv);
+	ft_print_params(argc, argv);
 	return (0);
 }
