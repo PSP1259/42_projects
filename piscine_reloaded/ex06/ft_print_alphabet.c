e031bfbbd03647ad3b1b8b62dbc4a0ab09916816
Philipp Spühler <214826864+PSP1259@users.noreply.github.com>
2026-09-08 09:30:49 +0200

    add ex09

---
+++ piscine_reloaded/ex06/ft_print_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pspuhler <pspuhler@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/07 14:16:02 by pspuhler          #+#    #+#             */
+/*   Updated: 2026/09/07 16:17:32 by pspuhler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		ft_putchar(c);
+		c++;
+	}
+}
+
+/*
+int	main(void)
+{
+	ft_print_alphabet();
+}
+*/
