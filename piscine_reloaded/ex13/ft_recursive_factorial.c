5e8bef4a90081b58bbdd448ab6061611e8db5f7a
Philipp Spühler <214826864+PSP1259@users.noreply.github.com>
2026-09-08 10:35:48 +0200

    add ex13

--- piscine_reloaded/ex13/ft_recursive_factorial.c
+++ piscine_reloaded/ex13/ft_recursive_factorial.c
@@ -6,10 +6,31 @@
 /*   By: pspuhler <pspuhler@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 09:54:24 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/09/08 09:56:48 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/08 10:35:26 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	
+	int	result;
+
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	result = nb * ft_recursive_factorial(nb - 1);
+	return (result);
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	result;
+
+	result = ft_recursive_factorial(5);
+	printf("%i", result);
+	return (0);
+}
+*/
