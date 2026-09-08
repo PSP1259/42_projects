c4eff33d6a44434db56f32bf4dade728f87395d5
Philipp Spühler <214826864+PSP1259@users.noreply.github.com>
2026-09-08 11:30:16 +0200

    secure int_max

--- piscine_reloaded/ex12/ft_iterative_factorial.c
+++ piscine_reloaded/ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: pspuhler <pspuhler@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 09:36:59 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/09/08 10:05:58 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/08 11:29:59 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_iterative_factorial(int nb)
 {
 	int	result;
 
-	if (nb < 0)
+	if (nb < 0 || nb > 12)
 		return (0);
 	if (nb == 0)
 		return (1);
