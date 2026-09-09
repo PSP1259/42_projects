c904f098cb7d423ed3b41035cdd9c56599b5e768
Philipp Spühler <214826864+PSP1259@users.noreply.github.com>
2026-09-09 12:34:33 +0200

    add ex21

--- piscine_reloaded/ex21/ft_range.c
+++ piscine_reloaded/ex21/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: pspuhler <pspuhler@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 16:20:14 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/09/08 16:47:08 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/09 12:34:21 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,14 @@
 
 int	*ft_range(int min, int max)
 {
-	long	diff;
-	int		*new;
-	long	i;
+	int	diff;
+	int	*new;
+	int	i;
 
+	if (min >= max)
+		return (NULL);
 	diff = max - min;
-	if (diff < 1)
-	{
-		new = NULL;
-		return (new);
-	}
-	new = malloc(diff * sizeof(int));
+	new = (int *)malloc(diff * sizeof(int));
 	if (!new)
 		return (NULL);
 	i = 0;
