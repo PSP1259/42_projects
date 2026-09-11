b75ac6248485cf036b9f3673e72fa936ecb67d61
Philipp Spühler <214826864+PSP1259@users.noreply.github.com>
2026-09-11 13:43:51 +0200

    add ex27

--- piscine_reloaded/ex27/ft_display_file.c
+++ piscine_reloaded/ex27/ft_display_file.c
@@ -6,40 +6,48 @@
 /*   By: pspuhler <pspuhler@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/11 11:55:16 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/09/11 13:03:36 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/11 13:43:17 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <fcntl.h>
 
-int	main(int argc, char **argv)
+int	ft_argc_check(int argc)
 {
-	int		fileDescriptor;
-	char	buffer[4096];
-	int		bytes_read;
-
 	if (argc > 2)
 	{
 		write(2, "Too many arguments.\n", 20);
-		return (1);
+		return (0);
 	}
 	if (argc < 2)
 	{
 		write(2, "File name missing.\n", 19);
-		return (1);
+		return (0);
 	}
-	fileDescriptor = open(argv[1], O_RDONLY);
-	if (fileDescriptor < 0)
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	int		filedescriptor;
+	char	buffer[4096];
+	int		bytes_read;
+
+	if (ft_argc_check(argc) == 0)
+		return (1);
+	filedescriptor = open(argv[1], O_RDONLY);
+	if (filedescriptor < 0)
 	{
 		write(2, "Cannot read file.\n", 18);
 		return (1);
 	}
-
-	while ((bytes_read = read(fileDescriptor, buffer, 4096)) > 0)
+	bytes_read = read(filedescriptor, buffer, 4096);
+	while (bytes_read > 0)
 	{
 		write(1, buffer, bytes_read);
+		bytes_read = read(filedescriptor, buffer, 4096);
 	}
-	close(fileDescriptor);
+	close(filedescriptor);
 	return (0);
 }
