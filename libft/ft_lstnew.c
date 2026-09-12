ab68bc6446783856fe9fd0aeeae7c6854d0b3161
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-09-12 09:36:58 +0200

    add ft_lstnew.c

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/14 19:35:16 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/08/30 18:16:28 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/12 09:36:44 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,34 +20,20 @@
 typedef struct s_list
 {
 	void			*content;
-	size_t			content_size;
 	struct s_list	*next;
 }					t_list;
 */
 
-t_list *ft_lstnew(void const *content, size_t content_size)
+t_list *ft_lstnew(void *content)
 {
-    t_list *node;
+    t_list *list;
 
-    if (!(node = (t_list *)malloc(sizeof(t_list))))
-        return (NULL);
-    if (!content)
-    {
-        node->content = NULL;
-        node->content_size = 0;
-    }
-    else
-    {
-        if (!(node->content = malloc(content_size)))
-        {
-            free(node);
-            return (NULL);
-        }
-        ft_memcpy(node->content, content, content_size);
-        node->content_size = content_size;
-    }
-    node->next = NULL;
-    return (node);
+	list = malloc(sizeof(t_list));
+	if (!list)
+		return (NULL);
+	list->content = content;
+	list->next = NULL;
+	return (list);
 }
 
 /*
@@ -56,11 +42,10 @@ int	main(void)
 	char	*str = "Hola Malaga";
 	t_list	*result;
 
-	result = ft_lstnew(str, 9);
+	result = ft_lstnew(str);
 	if (result)
 	{
 		printf("Content: %s\n", (char *)result->content);
-		printf("Size: %zu\n", result->content_size);
 		free(result);
 	}
 	return (0);
