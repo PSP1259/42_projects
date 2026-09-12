ad2755fcb15208b9b4c4aaa2bd927798492181d3
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-09-12 10:49:08 +0200

    add ft_lstiter.c

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,19 +6,53 @@
 /*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/31 16:34:06 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/08/31 16:37:38 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/12 10:48:44 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
+/*
+void	print_content(void *content)
+{
+	if (content)
+		printf("Node content: %s\n", (char *)content);
+}
+*/
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (!lst || !f)
 		return ;
 	while (lst)
 	{
-		f(lst);
+		f(lst->content);
 		lst = lst->next;
 	}
 }
+
+/*
+int	main(void)
+{
+	t_list	node1;
+	t_list	node2;
+	t_list	node3;
+
+	// Setup nodes and link them together
+	node1.content = "First Node";
+	node1.next = &node2;
+
+	node2.content = "Second Node";
+	node2.next = &node3;
+
+	node3.content = "Third Node";
+	node3.next = NULL;
+
+	// Iterate through the list and apply the print function
+	printf("Testing ft_lstiter:\n");
+	ft_lstiter(&node1, print_content);
+
+	return (0);
+}
+*/
