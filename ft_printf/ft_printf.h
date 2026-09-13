5b4df15e19c237bf4fde6d52314866efefa031ba
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-09-13 12:12:38 +0200

    norm

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,16 +6,16 @@
 /*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/01 15:42:37 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/09/01 18:19:06 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/13 12:12:27 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 #define FT_PRINTF_H
 
-#include "../libft/libft.h"
-#include <stdarg.h>
-#include <unistd.h>
+# include "../libft/libft.h"
+# include <stdarg.h>
+# include <unistd.h>
 
 int	ft_printf(const char *format, ...);
 int ft_print_char(va_list args);
