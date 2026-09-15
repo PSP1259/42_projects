397d110bb3e20515fb7c228743830a57e2608dd2
Philipp Spühler <214826864+PSP1259@users.noreply.github.com>
2026-09-15 16:51:17 +0200

    change variable name

--- ft_printf/ft_print_hex_lowercase.c
+++ ft_printf/ft_print_hex_lowercase.c
@@ -6,13 +6,13 @@
 /*   By: pspuhler <pspuhler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/13 20:48:58 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/09/14 14:18:35 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/14 16:05:23 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_put_hex_lower(unsigned int n)
+int	ft_put_hex_lowercase(unsigned int n)
 {
 	int		count;
 	char	*hex;
@@ -21,19 +21,19 @@ int	ft_put_hex_lower(unsigned int n)
 	hex = "0123456789abcdef";
 	count = 0;
 	if (n >= 16)
-		count += ft_put_hex_lower(n / 16);
+		count += ft_put_hex_lowercase(n / 16);
 	c = hex[n % 16];
 	write(1, &c, 1);
 	count++;
 	return (count);
 }
 
-int	ft_print_hex_lower(va_list args)
+int	ft_print_hex_lowercase(va_list args)
 {
 	int				count;
 	unsigned int	n;
 
 	n = va_arg(args, unsigned int);
-	count = ft_put_hex_lower(n);
+	count = ft_put_hex_lowercase(n);
 	return (count);
 }
