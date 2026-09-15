525303115361be332497a773a32951624d828746
Philipp Spühler <214826864+PSP1259@users.noreply.github.com>
2026-09-15 16:58:53 +0200

    structure header file

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pspuhler <pspuhler@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/15 15:34:28 by pspuhler          #+#    #+#             */
+/*   Updated: 2026/09/15 16:18:34 by pspuhler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+#ifndef BUFFER_SIZE                                                                 
+# define BUFFER_SIZE 10                                                             
+#endif
+
+char *get_next_line(int fd);
+
+#endif
