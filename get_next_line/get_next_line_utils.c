747b1a4865891ead67c9c395c59215e8c3a40859
Philipp Spühler <214826864+PSP1259@users.noreply.github.com>
2026-09-16 16:40:08 +0200

    add ft_strchr

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,8 +6,28 @@
 /*   By: pspuhler <pspuhler@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/15 16:19:32 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/09/15 16:19:35 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/16 16:39:13 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+char *ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	cc;
+
+	cc = (char)c;
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == cc)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
