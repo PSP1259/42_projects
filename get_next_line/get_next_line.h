352ac66629b8f06cd43f7f685d3a1fd461be56ac
PSP1259 <214826864+PSP1259@users.noreply.github.com>
2026-09-19 17:40:58 +0200

    add utils

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pspuhler <pspuhler@student.42malaga.c      +#+  +:+       +#+        */
+/*   By: pspuhler@student.42.fr <pspuhler>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/15 15:34:28 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/09/15 16:18:34 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/19 17:30:24 by pspuhler@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,14 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-#ifndef BUFFER_SIZE                                                                 
-# define BUFFER_SIZE 10                                                             
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
 #endif
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*strjoin_helper(char *stash, char *buffer, char *s3);
+char	*ft_strjoin(char *stash, char *buffer);
+int		ft_strlen(char *s);
 
 #endif
