bf0b2266c97854726fa22fe37c88b437b1688506
Philipp Spühler <214826864+PSP1259@users.noreply.github.com>
2026-09-21 17:53:44 +0200

    rename prototyp

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,23 +6,23 @@
 /*   By: pspuhler <pspuhler@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 10:33:35 by pspuhler          #+#    #+#             */
-/*   Updated: 2026/09/21 15:56:41 by pspuhler         ###   ########.fr       */
+/*   Updated: 2026/09/21 17:33:38 by pspuhler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stddef.h>
 
-void	ft_bzero(void *b, size_t len)
+void	ft_bzero(void *s, size_t n)
 {
 	unsigned char	*p;
 
-	p = (unsigned char *)b;
-	while (len > 0)
+	p = (unsigned char *)s;
+	while (n > 0)
 	{
 		*p = '\0';
 		p++;
-		len--;
+		n--;
 	}
 }
 
@@ -38,4 +38,4 @@ int	main(void)
 	printf("After: %s\n", &text[1]);
 	return (0);
 }
-/*
+*/
